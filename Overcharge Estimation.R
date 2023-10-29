0e137facd8eebcd4c2a05aab6d551512a1ba0d2c
Yzeghal <yanis.zeghal@ensae.fr>
2023-10-30 00:45:15 +0100

    Added a lot
    here

--- Overcharge Estimation.R
+++ Overcharge Estimation.R
@@ -91,6 +91,7 @@ legend("topright", legend=c("Price","e1","e2","e3"),
 
 dev.off()
 
+# 3D representation of controlling for a cost.
 angle = acos(cor(cost_,cartel))
 basis = rbind(c(-1,0,0), c(0,0,1))
 cart = c(0,1,0)
@@ -130,4 +131,95 @@ text3d(as.vector(first_proj)+c(0.6,0,0.05) , texts="Biased coefficient", color= 
 light3d(0,0)
 close3d()
 
+# Included variable bias with a dummy control
+# 1) Economist's model
 
+OC_uniform = 0.36 # uniform overcharge of 36% in the economist's model
+OC_specificG = 0.6 # overcharge when G=1 in reality
+OC_specfifcNoG= 0.3# overcharge when G=0 in reality
+P_increase = 0.1 # price increase that G=1 justifies
+OC_cost = 0.1 # E(cost|T=1) - E(cost|T=0)
+Pass_on = 0.8 #cost pass on rate
+Cost_inflationG = 0.1
+cartel = rep(0, 10000)
+cartel[1:5000] = 1
+G = rep(0,10000)
+G[3500:4500] =1
+cost_ = rep(0, 10000)
+cost_[1:5000] =  OC_cost 
+cost_[3500:4500] = OC_cost+Cost_inflationG
+cost_=cost_+eta #eta is directly attributed to cost for collinearity reasons
+Base_price = 1 # average price when T=0
+
+#price generation with uniform overcharge 
+price1 = Base_price + OC_uniform * cartel + Pass_on * cost_ + (P_increase)*G 
+
+#why controlling for G is justified
+lm(price1~cartel +cost_)
+lm(price1~cartel +cost_+G)
+
+plot(price1, type = "l", lwd = 3, col = "brown1", ylim = c(0,2.5), xlab = "Time",
+     ylab ="log(value)")
+lines(cost_, lwd = 3, col  = "cornflowerblue")
+lines(G, lwd = 2, col  = "black", lty = 3)
+
+
+abline(v = 5000, lwd = 2, lty = 3, col = "lightgrey")
+legend("topleft", legend=c("Price", "Cost","Dummy G"),
+       col=c("brown1", "cornflowerblue", "black"), cex=1.2,lty = c(1,1,3),lwd = c(2,2,2),
+       box.lty=1, box.lwd=2)
+textcartel = rbind(c(2000, 0.5), c(8000, 0.5))
+rownames(textcartel) = c("Cartel","No cartel")
+text(textcartel, rownames(textcartel), cex = 1.3)
+dev.off()
+
+# Economist's model fails when overcharge is not uniform (can be explained by G)
+price2 = Base_price + OC_specfifcNoG * cartel * (1-G) +OC_specificG * cartel *G + Pass_on * cost_ + P_increase *G
+lm(price2~cartel + cost_+G)
+
+plot(price2, type = "l", lwd = 3, col = "brown1", ylim = c(0,2.5), xlab = "Time",
+     ylab ="log(value)")
+lines(price1, lwd = 0.5, lty = 1, col = "grey70")
+lines(cost_, lwd = 3, col  = "cornflowerblue")
+lines(G, lwd = 2, col  = "black", lty = 3)
+abline(v = 5000, lwd = 2, lty = 3, col = "lightgrey")
+legend("topleft", legend=c("Price with specific overcharge when G=1", "Cost","Dummy G","Price with uniform overcharge 0.36"),
+       col=c("brown1", "cornflowerblue", "black", "grey70"), cex=0.9,lty = c(1,1,3,1),
+       lwd = c(2,2,2), box.lty=1, box.lwd=2)
+textcartel = rbind(c(2000, 0.5), c(8000, 0.5))
+rownames(textcartel) = c("Cartel","No cartel")
+text(textcartel, rownames(textcartel), cex = 1.3)
+dev.off()
+
+#Decomposition of the coefficient allocation determinants (see Annex, sect.4.3)
+#Different correlations when there is a special overcharge on product with G=1
+PxtG = lm(G~price+cost)$residuals #projection of G on Vect(1,X,T) orthogonal
+eta0 = lm(price2~cartel+cost)$residuals #residuals in reality
+etaEconomist = lm(price1~cartel+cost)$residuals #residuals if uniform overcharge
+cor(eta0, PxtG) #G is more efficient in reality
+cor(etaEconomist,PxtG) # Than in the expected case of uniform overcharge
+
+# What if G=1 is not included in T=1
+G[6000:10000] = 1
+cost_ = rep(0, 10000)
+cost_[cartel==1] =  OC_cost 
+cost_[G==1&cartel==1] = Cost_inflationG + OC_cost
+cost_[G==1&cartel==0] = Cost_inflationG
+cost_=cost_ #eta is directly attributed to cost for collinearity reasons 
+price3 = Base_price + OC_specfifcNoG * cartel * (1-G) +OC_specificG * cartel *G + Pass_on * cost_ + P_increase *G
+lm(price3~cartel + cost_+G)
+
+
+plot(price3, type = "l", lwd = 3, col = "brown1", ylim = c(0,2.5), xlab = "Time",
+     ylab ="log(value)")
+lines(cost_, lwd = 3, col  = "cornflowerblue")
+lines(G, lwd = 2, col  = "black", lty = 3)
+
+
+abline(v = 5000, lwd = 2, lty = 3, col = "lightgrey")
+legend("topleft", legend=c("Price", "Cost","Dummy G"),
+       col=c("brown1", "cornflowerblue", "black"), cex=1.2,lty = c(1,1,3),lwd = c(2,2,2),
+       box.lty=1, box.lwd=2)
+textcartel = rbind(c(2000, 0.5), c(8000, 0.5))
+rownames(textcartel) = c("Cartel","No cartel")
+text(textcartel, rownames(textcartel), cex = 1.3)
